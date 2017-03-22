3375c01b73efd3841c30da083fec93bf179996c5
abaybektursun <abaybektursun@gmail.com>
2017-03-22 01:09:36 +0000

    Test Fourier Transform

---
+++ source/normalization/CMakeFiles/Nomrmalization.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Nomrmalization.dir/main.cpp.o"
+  "Nomrmalization.pdb"
+  "Nomrmalization"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Nomrmalization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
