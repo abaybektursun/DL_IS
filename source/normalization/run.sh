a79fc8f1d2237c2314b7836a2869dc1c1ae61f69
abaybektursun <abaybektursun@gmail.com>
2017-03-23 05:06:51 +0000

    folders parsing

--- source/normalization/run.sh
+++ source/normalization/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+mkdir ../data/normalized
+img_counter=0
+find ../data/PH2Dataset/ -type f -iname "*.bmp" -print0 | while IFS= read -r -d $'\0' line; 
+do
+    ./Normalization "$line" ../data/normalized/${img_counter}.jpg
+    let img_counter=img_counter+1
+done
