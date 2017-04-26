326e2f9dc892dcaad4417c0ac3397cba70ee6f32
abaybektursun <abaybektursun@gmail.com>
2017-04-26 13:48:13 +0000

    trail 4

--- source/network/tran.sh
+++ source/network/tran.sh
@@ -2,16 +2,17 @@
 
 TOOLS=$CAFFE_HOME/build/tools
 
-#$TOOLS/caffe train \
-  #--solver=cifar_full_solver.prototxt
+time $TOOLS/caffe train \
+  --solver=cifar_full_solver.prototxt
+  --log_dir=/home/abaybektursun/IS/source/network/logs
 
 # reduce learning rate by factor of 10
-$TOOLS/caffe train \
-  --solver=cifar_full_solver_lr1.prototxt \
-  --snapshot=cifar_full_iter_60000.solverstate.h5
+#$TOOLS/caffe train \
+#  --solver=cifar_full_solver_lr1.prototxt \
+#  --snapshot=cifar_full_iter_60000.solverstate.h5
 
 # reduce learning rate again by factor of 10
-$TOOLS/caffe train \
-  --solver=cifar_full_solver_lr1.prototxt \
-  --snapshot=cifar_full_iter_65000.solverstate.h5
+#$TOOLS/caffe train \
+#  --solver=cifar_full_solver_lr2.prototxt \
+#  --snapshot=cifar_full_iter_65000.solverstate.h5
 
